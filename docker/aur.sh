801a246c7072a52917821e57397c50e1774e60ac
media <media@mediatmp.crtpoms.com>
2017-01-04 11:16:16 -0500

    New Files for docker

---
+++ docker/aur.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# install aur helper from github and then install app using helper
+if [[ ! -z "${aur_packages}" ]]; then
+    pacman -S --needed base-devel --noconfirm
+    curl -o "/tmp/apacman-any.pkg.tar.xz" -L "https://github.com/crtpoms/packages/raw/master/compiled/apacman-any.pkg.tar.xz"
+    pacman -U "/tmp/apacman-any.pkg.tar.xz" --noconfirm
+    set +e
+    apacman -S ${aur_packages} --noconfirm
+    exit_code=$?
+    set -e
+else
+    return 0
+fi
+
+if (( ${exit_code} != 0 && ${exit_code} != 1 )); then
+    echo "apacman returned exit code ${exit_code} (exit code 1 ignored), showing info for exit codes:-"
+    cat << EOF
+0   Success
+1   Miscellaneous errors
+2   Invalid parameters
+3   Fatal errors, not warnings
+4   No package matches found
+5   Package does not exist
+6   No internet connection
+7   No free space in tmpfs
+8   One or more package(s) failed to build, keep going
+9   One package failed to build, do not continue
+10  Permission problem −− fakeroot
+11  Permission problem −− root user
+12  Permission problem −− sudo
+13  Permission problem −− su
+EOF
+    # set return code to 1 to denote failure to build env
+    return 1
+fi
+
+# remove base devel excluding useful core packages
+pacman -Ru $(pacman -Qgq base-devel | grep -v pacman | grep -v sed | grep -v grep | grep -v gzip) --noconfirm
+
+# remove cached aur packages
+rm -rf "/var/cache/apacman/" || true
---
+++ docker/init.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+# send stdout and stderr to supervisor log file (to capture output from this script)
+exec 3>&1 4>&2 1>>/config/supervisord.log 2>&1
+
+echo "[info] System information $(uname -a)" | ts '%Y-%m-%d %H:%M:%.S'
+
+export PUID=$(echo "${PUID}" | sed -e 's/^[ \t]*//')
+if [[ ! -z "${PUID}" ]]; then
+    echo "[info] PUID defined as '${PUID}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+    echo "[warn] PUID not defined (via -e PUID), defaulting to '99'" | ts '%Y-%m-%d %H:%M:%.S'
+    export PUID="99"
+fi
+
+# set user nobody to specified user id (non unique)
+usermod -o -u "${PUID}" nobody &>/dev/null
+
+export PGID=$(echo "${PGID}" | sed -e 's/^[ \t]*//')
+if [[ ! -z "${PGID}" ]]; then
+    echo "[info] PGID defined as '${PGID}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+    echo "[warn] PGID not defined (via -e PGID), defaulting to '100'" | ts '%Y-%m-%d %H:%M:%.S'
+    export PGID="100"
+fi
+
+# set group users to specified group id (non unique)
+groupmod -o -g "${PGID}" users &>/dev/null
+
+# check for presence of perms file, if it exists then skip setting
+# permissions, otherwise recursively set on volume mappings for host
+if [[ ! -f "/config/perms.txt" ]]; then
+    echo "[info] Setting permissions recursively on volume mappings..." | ts '%Y-%m-%d %H:%M:%.S'
+    if [[ -d "/data" ]]; then
+        volumes=( "/config" "/data" )
+    else
+        volumes=( "/config" )
+    fi
+    set +e
+    chown -R "${PUID}":"${PGID}" "${volumes[@]}"
+    exit_code_chown=$?
+    chmod -R 775 "${volumes[@]}"
+    exit_code_chmod=$?
+    set -e
+    if (( ${exit_code_chown} != 0 || ${exit_code_chmod} != 0 )); then
+        echo "[warn] Unable to chown/chmod ${volumes}, assuming SMB mountpoint"
+    fi
+    echo "This file prevents permissions from being applied/re-applied to /config, if you want to reset permissions then please delete this file and restart the container." > /config/perms.txt
+else
+    echo "[info] Permissions already set for volume mappings" | ts '%Y-%m-%d %H:%M:%.S'
+fi
+
+# ENVVARS_PLACEHOLDER
+
+# PERMISSIONS_PLACEHOLDER
+
+# restore stdout/stderr (to prevent duplicate logging from supervisor)
+exec 1>&3 2>&4
+
+echo "[info] Starting Supervisor..." | ts '%Y-%m-%d %H:%M:%.S'
+exec /usr/bin/supervisord -c /etc/supervisor.conf -n
